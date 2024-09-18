1c4e785aaf367a26d9887c3483b6dc4b43cb1009
6353079417 <dhavalchauhan00321@gmail.com>
2024-09-18 16:34:50 +0530

    add another file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './index.css';
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Business from './components/Business';
+
 
 function App() {
   return (
    <>
         <Navbar/>
         <Header/>
+        <Business/>
    </>
   );
 }
---
+++ src/components/Business.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Business extends Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <h1 className='tw-text-center tw-text-5xl tw-font-bold tw-font-sans help'>What can we help you with?</h1>
+        <div className='Do-cir'>
+          <div className='round-cir'>Domain</div>
+          <div className='round-cir'>Recommended</div>
+          <div className='round-cir'>WordPress and security</div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div>
+          <img src="https://img1.wsimg.com/cdnassets/transform/ec1ec404-8337-4417-9c0a-a4bf5eeace3a/intl-img-hp-recore-domains-large-landscape" className='Do-sec' alt="" />
+          <div className='all-det'>          
+            <h1 className='tw-font-bold tw-text-2xl'>Domains</h1>
+            <p className='domain-pera'>Get started with the perfect domain, which comes with free domain privacy protection forever.</p>
+            <br />
+            <button className='Do-btn'>Search Domain</button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right right-arrow" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+</svg>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
