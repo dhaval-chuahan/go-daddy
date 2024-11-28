f5f7081f871de2784446a9809cefbc765d6278f1
6353079417 <dhavalchauhan00321@gmail.com>
2024-11-28 11:21:34 +0530

    add trust

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,15 +5,15 @@ export default class Slider extends Component {
         return (
             <div>
          {/* From Uiverse.io by JesusRafaelNavaCruz  */}
-         <div className='back-card'>
+         {/* <div className='back-card'>
                 <div className="container-slider">
                     <div className="card-slider">
-                    <img src="https://img1.wsimg.com/cdnassets/transform/c5ec380c-f411-41f0-8d20-eb469ad39b05/koartgallerytemplate1_desktop" alt="" className='title' />
+                    <img src="" alt="" />
                         
                     </div>
                     <div className="card-slider">
                         <p className="title">
-                            Morty Smith
+                            dhaval
                         </p>
                     </div>
                     <div className="card-slider">
@@ -64,7 +64,25 @@ export default class Slider extends Component {
                         </p>
                     </div>
                 </div>
-                </div>
+                </div> */}
+
+                <marquee behavior=""  direction="" scrollamount="150"  >
+                    <div className="tw-flex "  >
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/a4beaca9-862a-416c-a247-abc591413f9d/koretailfurnituretemplate_mobile" alt="" className='high h1' /></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/2024302c-7976-4f9a-b878-578f3d2fc2dc/kobookstoretemplate_desktop" alt="" className='high h2' /></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/bc4b4bee-f709-4f8b-b606-a92a74286ae2/Raheim-Thompson-site" alt="" className='high h3' /></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/5be08aac-da26-4a95-816b-e5b5abe1c46b/retailsingleproduct_mobile" alt="" className='high h4' /></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/c5ec380c-f411-41f0-8d20-eb469ad39b05/koartgallerytemplate1_desktop" alt="" className='high h5' /></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/cfb06d21-498d-4421-9355-167d720e015b/kohomehousewarestemplate_desktop" alt="" className='high h6' /></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <div><img src="https://img1.wsimg.com/cdnassets/transform/d196e84d-26d6-47d0-b4df-b3e493a3934c/kohealthalternativetemplate_mobile" alt="" className='high h7' /></div>
+                    </div>
+                </marquee>
             </div>
         )
     }
---
+++ src/components/Trust.jsx
@@ -0,0 +1,197 @@
+import React, { Component } from 'react'
+
+export default class Trust extends Component {
+  render() {
+    return (
+      <div>
+        <p className='tw-text-4xl tw-font-bold write'>Trusted by 20+ million customers around the world.</p>
+        <br /><br />
+        <div className='back-card'>
+                <div className="container-slider-1">
+                    <div className="card-slider-1">
+                    <p className="title-1">
+                            THE SPORT PERSON HAVE GOOD KNOWLEDGE..
+                            <br />
+                            <br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            The support person have good knowledge and help with good suggestion on improving my setting that is extraordinary.
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+
+                            <p>Niraj singh</p>
+                            
+    
+                        </p>                                   
+                    </div>
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                            10 ON 10 CUSTOMER SUPPORT.
+                            <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            The support provided by GoDaddy is the best in the industry with knowledgeable customer agents and best practices. They offer the best rates upfront over the call.
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                                   
+                            <p>Chandrasekar Mhsakar</p>
+                        </p>
+                    </div>
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                            GOOD COMMUNICATION SKILL BY SUPPORT ASSISTANT 
+                            <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            Great Product and Service Customer Support Assistance from Godadddy Support Team                            
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <p>Prashant Khambhatiya</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                            GODADDY CUSTOMER SUPPORT IS 100%..
+                            <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            Godaddy customer support is 100% reliable for all website-related support. The support staff are reliable, quick, knowledgeable, and always ready to help.                           
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            
+                            <p>Sirisha Ramaraju</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                            EXCELLENT SERVICES GODADDY.
+                            <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            Very Nice Service.CustmerCare People are So Polite.Good Offers.Clarifed Each And Every Point, Discounts Also.Finally Happy with the GoDaddy Support.                   
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                           
+                            <p>KAPU CARZ HUB</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                        GODADDY REALLY GOES!
+                        <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            Easiest and smooth experience for website and domain registration/hosting. The customer service is amazing in troubleshooting and resolving issues: always going all the way to ensure we get the best.                  
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            
+                            
+                            <p>Kannan Rajaram</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                        POLITE AND HELPFUL CUSTOMER SERVICES
+                        <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            The customer support at GoDaddy is extremely polite and helpful. I get my problems sorted out in quick time. Thanks!                  
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <p>Prashant Mishra</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                        EXCELLENT PRODUCT AND THE SERVICES IS..
+                        <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            Excellent product and the service is top notch. Very happy to be associated with GODaddy!                  
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <p>Vishal Hanmattekar</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                            I HAVE BEEN USING USING GODADDY HOSTING
+                            <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            I have been using Godaddy Hosting Services for more than 10 years. It's very easy to use and the Customer service is excellent.              
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            
+                            <p>Jamal Ashraf</p>
+                        </p>
+                    </div>
+
+                    <div className="card-slider-1">
+                        <p className="title-1">
+                            TOP LEVEL PROFESSIONALISM
+                            <br /><br /><br />
+                            <p className="tw-text-2xl tw-w-80">
+                            After trying 2-3 domain/web hosting companies, I found GoDaddy much more professional in dealing with customer issues. Highly recommended.              
+                            </p>
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                            <br />
+                           
+                            <p>Chandra Shekhar</p>
+                        </p>
+                    </div>
+                </div>
+                </div>
+      </div>
+    )
+  }
+}
